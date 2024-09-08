afa57b28352c3273fddd5a62ccd236ca6b12d3c9
Josh Newman <josh.newman4390@gmail.com>
2024-09-08 11:14:31 +1000

    Redirect to app store

---
+++ gatsby-node.js
@@ -0,0 +1,9 @@
+exports.createPages = async ({ graphql, actions }) => {
+  const { createRedirect } = actions;
+
+  createRedirect({
+    fromPath: "/mobile",
+    toPath: "https://apps.apple.com/au/app/fishnett/id6599842071",
+    isPermanent: true,
+  });
+};
