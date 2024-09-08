132370b2da73b0086b4598db57f9bff96758f725
Josh Newman <josh.newman4390@gmail.com>
2024-09-08 11:29:15 +1000

    Add trailing slash

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createRedirect } = actions;
 
   createRedirect({
-    fromPath: "/mobile",
+    fromPath: "/mobile/",
     toPath: "https://apps.apple.com/au/app/fishnett/id6599842071",
     isPermanent: true,
   });
